849e9e064add635086a95ea25292426c16ca034d
soccermoe14 <114617209+soccermoe14@users.noreply.github.com>
2022-11-12 19:36:45 -0600

    completed to best ability

--- index_test.js
+++ index_test.js
@@ -6,6 +6,14 @@ describe('MyFunctions', function() {
             it ('should divide a deck of cards into two even decks (ex: 52 cards should becoe 2 decks of 26', function(){
                 
                 var x = dealDeck();
-                expect(x).to.equal()
-            });
-            
+                expect(x).to.equal(
+                    let p1 = new Player();
+let p2 = new Player();
+
+function dealDeck(warDeck) { //function to divide half of the shuffled cards evenly amognst players 1 & 2
+    let dividedDeck = Math.ceil(warDeck.cards.length / 2);
+p1.hand = warDeck.cards.slice(0, dividedDeck);
+p2.hand = warDeck.cards.slice(-dividedDeck);
+        });
+    });
+});
